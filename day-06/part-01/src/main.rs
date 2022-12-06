9c242619acc06869b6017a32083bbb3e93829b73
Joe Echtenkamp <github@echtenkamp.com>
2022-12-06 17:15:35 -0600

    Day-06 complete

--- day-06/part-01/src/main.rs
+++ day-06/part-01/src/main.rs
@@ -1,30 +1,46 @@
-use std::collections::HashSet;
-
-use regex::Regex;
-
 fn main() -> anyhow::Result<()> {
     let mut lines = include_str!("../../input").lines();
-    let line_input=lines.next().unwrap();
+    let line_input = lines.next().unwrap();
+    let line_length = line_input.len();
+    let mut start_of_packet_marker_index: usize = 4;
+    let inter = line_input.chars().collect::<Vec<char>>();
+    let mut windows = inter.windows(4);
 
-    let marker:Vec<char>=Vec::new();
-    let mut index:i32 =0;
-    for char_input in line_input.chars() {
-        index=index+1;
+    while start_of_packet_marker_index < (line_length - 4) {
+        let start_of_packet_marker = windows.next().unwrap();
+        if is_marker(start_of_packet_marker) {
+            break;
+        }
 
-        
+        start_of_packet_marker_index += 1;
     }
 
-  //  println!("Index of first message {0}", char_set.len());
+    // 1175 is the correct answer
+    println!("Index of first message {0}", start_of_packet_marker_index);
     Ok(())
 }
 
-fn is_marker(pot_mark:&Vec<char>)-> bool {
-
-    if  pot_mark.len() < 4 {
+fn is_marker(pot_mark: &[char]) -> bool {
+    if pot_mark[0] == pot_mark[1] {
+        return false;
+    }
+    if pot_mark[0] == pot_mark[2] {
+        return false;
+    }
+    if pot_mark[0] == pot_mark[3] {
         return false;
     }
 
-    let v: Vec<_> = pot_mark.clone().into_iter().unique().collect();
+    if pot_mark[1] == pot_mark[2] {
+        return false;
+    }
+    if pot_mark[1] == pot_mark[3] {
+        return false;
+    }
+
+    if pot_mark[2] == pot_mark[3] {
+        return false;
+    }
 
     return true;
 }
--- day-06/part-02/src/main.rs
+++ day-06/part-02/src/main.rs
@@ -1,170 +1,40 @@
-use regex::Regex;
+use std::collections::HashSet;
 
 fn main() -> anyhow::Result<()> {
-    let lines = include_str!("../../input").lines();
-    //     [G] [R]                 [P]
-    //     [H] [W]     [T] [P]     [H]
-    //     [F] [T] [P] [B] [D]     [N]
-    // [L] [T] [M] [Q] [L] [C]     [Z]
-    // [C] [C] [N] [V] [S] [H]     [V] [G]
-    // [G] [L] [F] [D] [M] [V] [T] [J] [H]
-    // [M] [D] [J] [F] [F] [N] [C] [S] [F]
-    // [Q] [R] [V] [J] [N] [R] [H] [G] [Z]
-    //  1   2   3   4   5   6   7   8   9
+    let mut lines = include_str!("../../input").lines();
+    let line_input = lines.next().unwrap();
+    let line_length = line_input.len();
+    let mut start_of_packet_marker_index: usize = 14;
+    let inter = line_input.chars().collect::<Vec<char>>();
+    let mut windows = inter.windows(14);
 
-    // Start on line 11
-    let mut stacks: Vec<Vec<char>> = Vec::new();
-
-    // Stack 01
-    let mut stack: Vec<char> = Vec::new();
-    stack.push('Q');
-    stack.push('M');
-    stack.push('G');
-    stack.push('C');
-    stack.push('L');
-    stacks.push(stack);
-
-    // Stack 02
-    stack = Vec::new();
-    stack.push('R');
-    stack.push('D');
-    stack.push('L');
-    stack.push('C');
-    stack.push('T');
-    stack.push('F');
-    stack.push('H');
-    stack.push('G');
-    stacks.push(stack);
-
-    // Stack 03
-    stack = Vec::new();
-    stack.push('V');
-    stack.push('J');
-    stack.push('F');
-    stack.push('N');
-    stack.push('M');
-    stack.push('T');
-    stack.push('W');
-    stack.push('R');
-    stacks.push(stack);
-
-    // Stack 04
-    stack = Vec::new();
-    stack.push('J');
-    stack.push('F');
-    stack.push('D');
-    stack.push('V');
-    stack.push('Q');
-    stack.push('P');
-    stacks.push(stack);
-
-    // Stack 05
-    stack = Vec::new();
-    stack.push('N');
-    stack.push('F');
-    stack.push('M');
-    stack.push('S');
-    stack.push('L');
-    stack.push('B');
-    stack.push('T');
-    stacks.push(stack);
-
-    // Stack 06
-    stack = Vec::new();
-    stack.push('R');
-    stack.push('N');
-    stack.push('V');
-    stack.push('H');
-    stack.push('C');
-    stack.push('D');
-    stack.push('P');
-    stacks.push(stack);
-
-    // Stack 07
-    stack = Vec::new();
-    stack.push('H');
-    stack.push('C');
-    stack.push('T');
-    stacks.push(stack);
-
-    // Stack 08
-    stack = Vec::new();
-    stack.push('G');
-    stack.push('S');
-    stack.push('J');
-    stack.push('V');
-    stack.push('Z');
-    stack.push('N');
-    stack.push('H');
-    stack.push('P');
-    stacks.push(stack);
-
-    // Stack 09
-    stack = Vec::new();
-    stack.push('Z');
-    stack.push('F');
-    stack.push('H');
-    stack.push('G');
-    stacks.push(stack);
-
-    for line in lines {
-        // the first 10-ish lines are the setup for the stacks
-        // move 5 from 8 to 2
-        if !line.starts_with("move") {
-            continue;
+    while start_of_packet_marker_index < (line_length - 14) {
+        let start_of_packet_marker = windows.next().unwrap();
+        // println!("{:#?}", start_of_packet_marker);
+        if is_marker(start_of_packet_marker) {
+            break;
         }
 
-        let re = Regex::new(r"move ([0-9]+) from ([0-9]+) to ([0-9]+)").unwrap();
-        let caps = re.captures(line).unwrap();
-        
-        let qty_str = caps.get(1).map_or("", |m| m.as_str());
-        let from_str = caps.get(2).map_or("", |m| m.as_str());
-        let to_str = caps.get(3).map_or("", |m| m.as_str());
-
-        let qty:i32 = qty_str.parse().unwrap();
-
-        let from:i32 = (from_str.parse::<i32>().unwrap()) - 1;
-        let to:i32 = (to_str.parse::<i32>().unwrap()) - 1;
-
-        if qty > stacks[from as usize].len() as i32 {
-            println!("Gonna pop too many boxes!")
-        }
-
-        println!("Moving {0} boxes from {1} to {2}", qty,from_str,to_str);
-        
-        let mut items:Vec<char>=Vec::new();
-        for i in 1..(qty+1){
-            println!("Moving box {0}", i);    
-            let item:char = stacks[from as usize].pop().unwrap();
-            items.push(item);
-        }
-        for i in 1..(qty+1){
-            println!("Moving box {0}", i);    
-            let item:char = items.pop().unwrap();
-            stacks[to as usize].push(item);
-        }
-
-
-        //print_stacks(&stacks);
+        start_of_packet_marker_index += 1;
     }
 
-    println!("Crates on Top:");
-    for i in 0..9{
-        print!("{0}",stacks[i].pop().unwrap());
-    }
-    println!("");
-
-    // Correct Answer: GCFGLDNJZ
+    // 3217 is the correct answer
+    println!("Index of first message {0}", start_of_packet_marker_index);
     Ok(())
 }
 
-fn print_stacks( cargo_stacks:&Vec<Vec<char>>){
-    println!("Stacks State");
-    for i in 0..cargo_stacks.len(){
-        print!("Stack-{0} - ",i);
-        for j in 0..cargo_stacks[i].len(){
-            print!("{0}",cargo_stacks[i][j]);
-        }   
-        println!();
+fn is_marker(pot_mark: &[char]) -> bool {
+    let mut hashset: HashSet<char> = HashSet::new();
+    for c in pot_mark {
+        if !hashset.insert(c.clone()) {
+            return false;
+        }
     }
+
+    if hashset.len() < 14 {
+        // println!("{:#?}", hashset);
+        return false;
+    }
+
+    return true;
 }
