db37892c790cad7920206f9724fadfb7b5f55044
Joe Echtenkamp <github@echtenkamp.com>
2022-12-09 06:51:36 -0600

    day 07 progress

--- day-07/part-01/src/main.rs
+++ day-07/part-01/src/main.rs
@@ -1,8 +1,10 @@
+use std::collections::HashMap;
+
 use regex::Regex;
 
 fn main() -> anyhow::Result<()> {
     let lines = include_str!("../../input").lines();
-  let mut current_path="";
+  let mut current_path="".to_string();
 
   for line in lines{
     if line.starts_with("$"){
@@ -12,18 +14,23 @@ fn main() -> anyhow::Result<()> {
         let cmd = caps.get(1).map_or("", |m| m.as_str());
         let arg = caps.get(2).map_or("", |m| m.as_str());
         
-        if(cmd=="ls"){
+        if cmd=="ls" {
             continue;
         }
 
-        if(cmd == "cd"){
+        if cmd == "cd" {
             if arg==".."{
                 let mut current_path_parts = current_path.split("/").collect::<Vec<&str>>();
                 
                 // drop the last folder
                 current_path_parts.truncate(current_path_parts.len()-1);
+                let temp = current_path_parts.iter().map(|i| i.to_string()).collect::<Vec<String>>();
+                
+                current_path = temp.join("/");
+                
 
-                current_path = current_path_parts.join(&(String::from( "/")));
+
+
             }
         }
 
@@ -36,6 +43,6 @@ fn main() -> anyhow::Result<()> {
 struct Directory{
     name: String,
     parent: String ,
-    children: Vec<String>
-
+    child_dirs: Vec<Directory>,
+    files: HashMap<String,i32>
 }
