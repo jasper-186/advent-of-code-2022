c67738247176494fa0535f8af14bc0d974ca3aa8
Joe Echtenkamp <github@echtenkamp.com>
2022-12-15 10:38:38 -0600

    part-01 complete

--- day-09/part-01/src/main.rs
+++ day-09/part-01/src/main.rs
@@ -13,9 +13,12 @@ fn main() -> anyhow::Result<()> {
     all_tail_positions.insert(tail_position);
     for line in lines {
         // parse line
-        let line_chars: Vec<char> = line.chars().collect();
-        let distance = line_chars[2].to_digit(10).unwrap();
+        let line_chars: Vec<&str> = line.split_ascii_whitespace().collect();
+        
+        let distance:u32 = line_chars[1].parse().unwrap();
 
+
+        println!("{0} -> {1} {2}",line, line_chars[0],distance);
         for _ in 0..distance {
             // remember previous head position
             original_head_position = head_position;
@@ -24,9 +27,28 @@ fn main() -> anyhow::Result<()> {
             head_position = move_head(line_chars[0], head_position);
 
             // move tail?
-            if does_tail_move(head_position, tail_position) {
+            if does_tail_move(&head_position, &tail_position) {
+               
+                // println!(
+                //     "Head ({0},{1}) Tail ({2},{3}) -> ({4},{5})",
+                //     head_position.0,
+                //     head_position.1,
+                //     tail_position.0,
+                //     tail_position.1,                    
+                //     original_head_position.0,
+                //     original_head_position.1,
+                // );
                 tail_position = original_head_position;
                 all_tail_positions.insert(tail_position);
+                
+            }else{
+                // println!(
+                //     "Head ({0},{1}) Tail ({2},{3})",
+                //     head_position.0,
+                //     head_position.1,
+                //     tail_position.0,
+                //     tail_position.1,
+                // );
             }
         }
     }
@@ -39,7 +61,7 @@ fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-fn does_tail_move(head: (i32, i32), tail: (i32, i32)) -> bool {
+fn does_tail_move(head: &(i32, i32), tail: &(i32, i32)) -> bool {
     let x_dist = ( head.0-tail.0).abs();
     let y_dist = ( head.1-tail.1).abs();
 
@@ -54,19 +76,24 @@ fn does_tail_move(head: (i32, i32), tail: (i32, i32)) -> bool {
     return false;
 }
 
-fn move_head(direction: char, current_position: (i32, i32)) -> (i32, i32) {
+fn move_head(direction: &str, current_position: (i32, i32)) -> (i32, i32) {
+    // println!(
+    //     "{0}",
+    //     direction
+    // );
+
     match direction{
-        'U'=>{
+        "U"=>{
             return (current_position.0, (current_position.1+1))
         },
-        'D'=>{
+        "D"=>{
             return (current_position.0, (current_position.1-1))
         },
-        'L'=>{
-            return (current_position.0-1, current_position.1)
+        "L"=>{
+            return ((current_position.0-1), current_position.1)
         },
-        'R'=>{
-            return (current_position.0+1, current_position.1)
+        "R"=>{
+            return ((current_position.0+1), current_position.1)
         },
         _=>{
             return current_position;
