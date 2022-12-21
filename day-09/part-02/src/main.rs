6c2c585a222ca19845aa3492dfed6944b8f748a4
Joe Echtenkamp <github@echtenkamp.com>
2022-12-21 07:01:49 -0600

    Still broken

--- day-09/part-02/src/main.rs
+++ day-09/part-02/src/main.rs
@@ -5,7 +5,7 @@ fn main() -> anyhow::Result<()> {
     let mut all_tail_positions: HashSet<(i32, i32)> = HashSet::new();
 
     //let mut head_position: (i32, i32) = (0, 0);
-    let mut original_head_position: (i32, i32);
+    //let mut original_head_position: (i32, i32);
     //let mut tail_position: (i32, i32) = (0, 0);
 
     let mut rope: Vec<(i32, i32)> = Vec::new();
@@ -24,7 +24,7 @@ fn main() -> anyhow::Result<()> {
         //println!("{0} -> {1} {2}", line, line_chars[0], distance);
         for _ in 0..distance {
             // remember previous head position
-            original_head_position = rope[0];
+           // original_head_position = rope[0];
 
             // move head
             rope[0] = move_head(line_chars[0], rope[0]);
@@ -32,9 +32,8 @@ fn main() -> anyhow::Result<()> {
             for i in 1..10 {
                 // move tail?
                 if does_tail_move(&rope[i - 1], &rope[i]) {
-                    let temp = rope[i];
-                    rope[i] = original_head_position;
-                    original_head_position = temp;
+                    let temp = move_tail(&rope[i-1], &rope[i]);
+                     rope[i]=temp;
                     if i == 9 {
                         println!("({0},{1})", rope[i].0,rope[i].1);
                         all_tail_positions.insert(rope[i]);
@@ -47,6 +46,7 @@ fn main() -> anyhow::Result<()> {
     }
 
     // 4883 is too high
+    // 14 is not the right answer
     println!(
         "The tail of the rope visit {0} positions at least one time",
         all_tail_positions.len()
@@ -84,4 +84,34 @@ fn move_head(direction: &str, current_position: (i32, i32)) -> (i32, i32) {
             return current_position;
         }
     };
+}
+
+fn move_tail(head: &(i32, i32), tail: &(i32, i32)) -> (i32, i32) {
+    // let x_dist = (head.0 - tail.0)/2;
+    // let y_dist = (head.1 - tail.1)/2;
+
+    // return (tail.0+x_dist, tail.1+y_dist);
+
+    let x_dist = (head.0 - tail.0);
+    let y_dist = (head.1 - tail.1);
+
+
+    if x_dist > 1 {
+        return (tail.0+1,tail.1);
+    }
+
+    if y_dist > 1 {
+        return (tail.0,tail.1+1);
+    }
+
+    if x_dist < -1 {
+        return (tail.0-1,tail.1);
+    }
+
+    if y_dist < -1 {
+        return (tail.0,tail.1-1);
+    }
+
+    return (tail.0,tail.1);
+   // return false;
 }
