55e997117db40b5af44fbcc6020ca12a7d499294
Joe Echtenkamp <github@echtenkamp.com>
2023-04-08 09:21:16 -0500

    completed Day 09 with Help from Techtenk

--- day-09/part-02/src/main.rs
+++ day-09/part-02/src/main.rs
@@ -1,7 +1,7 @@
 use std::collections::HashSet;
 
 fn main() -> anyhow::Result<()> {
-    let lines = include_str!("../../test-input-large").lines();
+    let lines = include_str!("../../input").lines();
     let mut all_tail_positions: HashSet<(i32, i32)> = HashSet::new();
 
     //let mut head_position: (i32, i32) = (0, 0);
@@ -21,7 +21,7 @@ fn main() -> anyhow::Result<()> {
 
         let distance: u32 = line_chars[1].parse().unwrap();
 
-        //println!("{0} -> {1} {2}", line, line_chars[0], distance);
+        println!("{0} -> {1} {2}", line, line_chars[0], distance);
         for _ in 0..distance {
             // remember previous head position
            // original_head_position = rope[0];
@@ -29,24 +29,27 @@ fn main() -> anyhow::Result<()> {
             // move head
             rope[0] = move_head(line_chars[0], rope[0]);
 
+            //println!("({0},{1})", rope[0].0,rope[0].1);
             for i in 1..10 {
                 // move tail?
                 if does_tail_move(&rope[i - 1], &rope[i]) {
                     let temp = move_tail(&rope[i-1], &rope[i]);
                      rope[i]=temp;
                     if i == 9 {
-                        println!("({0},{1})", rope[i].0,rope[i].1);
-                        all_tail_positions.insert(rope[i]);
+                        println!("({0},{1})", rope[9].0,rope[9].1);
+                        all_tail_positions.insert(rope[9]);
                     }
                 } else {
                     break;
                 }
             }
         }
+        //println!("{:?}", rope);
+        
     }
 
-    // 4883 is too high
-    // 14 is not the right answer
+    // 2449 is the Correct Answer
+    
     println!(
         "The tail of the rope visit {0} positions at least one time",
         all_tail_positions.len()
@@ -70,11 +73,7 @@ fn does_tail_move(head: &(i32, i32), tail: &(i32, i32)) -> bool {
 }
 
 fn move_head(direction: &str, current_position: (i32, i32)) -> (i32, i32) {
-    // println!(
-    //     "{0}",
-    //     direction
-    // );
-
+   
     match direction {
         "U" => return (current_position.0, (current_position.1 + 1)),
         "D" => return (current_position.0, (current_position.1 - 1)),
@@ -87,15 +86,31 @@ fn move_head(direction: &str, current_position: (i32, i32)) -> (i32, i32) {
 }
 
 fn move_tail(head: &(i32, i32), tail: &(i32, i32)) -> (i32, i32) {
-    // let x_dist = (head.0 - tail.0)/2;
-    // let y_dist = (head.1 - tail.1)/2;
+    let x_dist = head.0 - tail.0;
+    let y_dist = head.1 - tail.1;
 
-    // return (tail.0+x_dist, tail.1+y_dist);
+    // Diagonally 
+    // (Up/Right)
+    if x_dist > 0 && y_dist > 0 {
+        return (tail.0+1, tail.1+1);
+    }
+    
+    // (Down/Left)
+    if x_dist < 0 && y_dist < 0  {
+        return (tail.0-1, tail.1-1);
+    }
 
-    let x_dist = (head.0 - tail.0);
-    let y_dist = (head.1 - tail.1);
+    // (Up/Left)
+    if x_dist < 0 && y_dist > 0 {
+        return (tail.0-1, tail.1+1);
+    }
 
+    // (Down/Right)
+    if x_dist > 0 && y_dist < 0  {
+        return (tail.0+1, tail.1-1);
+    }
 
+    // Orthagonally
     if x_dist > 1 {
         return (tail.0+1,tail.1);
     }
@@ -112,6 +127,5 @@ fn move_tail(head: &(i32, i32), tail: &(i32, i32)) -> (i32, i32) {
         return (tail.0,tail.1-1);
     }
 
-    return (tail.0,tail.1);
-   // return false;
+    return (tail.0,tail.1);   
 }
