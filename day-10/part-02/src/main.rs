a2883a0955b7ab17c54bcdd40c5fc84bf1c3eb38
Joe Echtenkamp <github@echtenkamp.com>
2023-04-10 14:08:52 -0500

    Finished Part 02

--- day-10/part-02/src/main.rs
+++ day-10/part-02/src/main.rs
@@ -1,131 +1,86 @@
-use std::collections::HashSet;
+
+#[derive(PartialEq, Eq)]
+enum Instruction{
+    None =-1,
+    Noop = 0,
+    Addx = 2
+}
 
 fn main() -> anyhow::Result<()> {
-    let lines = include_str!("../../input").lines();
-    let mut all_tail_positions: HashSet<(i32, i32)> = HashSet::new();
-
-    //let mut head_position: (i32, i32) = (0, 0);
-    //let mut original_head_position: (i32, i32);
-    //let mut tail_position: (i32, i32) = (0, 0);
-
-    let mut rope: Vec<(i32, i32)> = Vec::new();
-    for i in 0..10 {
-        rope.insert(i, (0, 0));
-    }
-
-    // insert the orginal tail position
-    all_tail_positions.insert((0, 0));
-    for line in lines {
-        // parse line
-        let line_chars: Vec<&str> = line.split_ascii_whitespace().collect();
-
-        let distance: u32 = line_chars[1].parse().unwrap();
-
-        println!("{0} -> {1} {2}", line, line_chars[0], distance);
-        for _ in 0..distance {
-            // remember previous head position
-           // original_head_position = rope[0];
-
-            // move head
-            rope[0] = move_head(line_chars[0], rope[0]);
-
-            //println!("({0},{1})", rope[0].0,rope[0].1);
-            for i in 1..10 {
-                // move tail?
-                if does_tail_move(&rope[i - 1], &rope[i]) {
-                    let temp = move_tail(&rope[i-1], &rope[i]);
-                     rope[i]=temp;
-                    if i == 9 {
-                        println!("({0},{1})", rope[9].0,rope[9].1);
-                        all_tail_positions.insert(rope[9]);
-                    }
-                } else {
-                    break;
-                }
-            }
-        }
-        //println!("{:?}", rope);
-        
-    }
-
-    // 2449 is the Correct Answer
+    let mut lines = include_str!("../../input").lines();
     
-    println!(
-        "The tail of the rope visit {0} positions at least one time",
-        all_tail_positions.len()
-    );
+    // Cycle 1
+    let mut v_register = 0;
+    // Cycle 2
+    let mut w_register = 0;
+    // Register
+    let mut x_register = 1;
+    let mut current_operation = Instruction::None;
+    let mut cycle = 1;
+    let mut run_program=true;
+
+    while run_program{
+        // get the value of x at 20 units, and every 40  units there after
+        if cycle%40 == 1{
+            print!("\n")
+        }
+
+        // AT the Start of the Cycle, are we currently executing a previous execution?
+        //  If not, read the next instruction
+        if current_operation == Instruction::None{
+            let next_operation = lines.next();      
+            if next_operation.is_none(){
+                run_program=false;
+                continue;
+            }  
+            let mut parts = next_operation.unwrap().split_ascii_whitespace();
+            let operation = parts.next().unwrap();
+            if operation.to_uppercase() == "ADDX"{
+                let amount_str = parts.next();
+                let amount: i32 = amount_str.unwrap().parse().unwrap();
+                v_register = amount;
+                current_operation = Instruction::Addx;
+            } else if operation.to_uppercase() == "NOOP"{
+                current_operation = Instruction::Noop;
+            }
+    
+        }
+
+        let pixel_column = ((cycle)%40)-1;
+        let sprite_low = x_register-1;
+        let sprite_high = x_register+1;
+        if sprite_low<=pixel_column && pixel_column<=sprite_high {
+            print!("x");
+        }else{
+            print!(".")
+        }
+
+        // Move v->w, and w->x
+        if current_operation == Instruction::Addx {
+            // this should happen after the 2nd cycle of a instruction
+            if w_register != 0 {
+                x_register += w_register;
+                w_register = 0;
+                // adding this to the x_reg completes the operation, so clear the thing
+                current_operation=Instruction::None;
+            }
+
+            // this should happen in the same cycle as the initial instruction add
+            if v_register != 0 {
+                w_register = v_register;
+                v_register = 0;
+            }
+    
+        }
+
+        cycle+=1;
+        // doing this because i expect part 2 to use NOOP somehow
+        if current_operation == Instruction::Noop {
+            current_operation = Instruction::None;
+        }
+
+
+    }
     Ok(())
 }
 
-fn does_tail_move(head: &(i32, i32), tail: &(i32, i32)) -> bool {
-    let x_dist = (head.0 - tail.0).abs();
-    let y_dist = (head.1 - tail.1).abs();
-
-    if x_dist > 1 {
-        return true;
-    }
-
-    if y_dist > 1 {
-        return true;
-    }
-
-    return false;
-}
-
-fn move_head(direction: &str, current_position: (i32, i32)) -> (i32, i32) {
-   
-    match direction {
-        "U" => return (current_position.0, (current_position.1 + 1)),
-        "D" => return (current_position.0, (current_position.1 - 1)),
-        "L" => return ((current_position.0 - 1), current_position.1),
-        "R" => return ((current_position.0 + 1), current_position.1),
-        _ => {
-            return current_position;
-        }
-    };
-}
-
-fn move_tail(head: &(i32, i32), tail: &(i32, i32)) -> (i32, i32) {
-    let x_dist = head.0 - tail.0;
-    let y_dist = head.1 - tail.1;
-
-    // Diagonally 
-    // (Up/Right)
-    if x_dist > 0 && y_dist > 0 {
-        return (tail.0+1, tail.1+1);
-    }
-    
-    // (Down/Left)
-    if x_dist < 0 && y_dist < 0  {
-        return (tail.0-1, tail.1-1);
-    }
-
-    // (Up/Left)
-    if x_dist < 0 && y_dist > 0 {
-        return (tail.0-1, tail.1+1);
-    }
-
-    // (Down/Right)
-    if x_dist > 0 && y_dist < 0  {
-        return (tail.0+1, tail.1-1);
-    }
-
-    // Orthagonally
-    if x_dist > 1 {
-        return (tail.0+1,tail.1);
-    }
-
-    if y_dist > 1 {
-        return (tail.0,tail.1+1);
-    }
-
-    if x_dist < -1 {
-        return (tail.0-1,tail.1);
-    }
-
-    if y_dist < -1 {
-        return (tail.0,tail.1-1);
-    }
-
-    return (tail.0,tail.1);   
-}
